008ef7f9bd2fd35665add4b19e151753d32fc657
Goran <gogecmaestrotib92@gmail.com>
2025-11-28 17:31:05 +0100

    Fix ESLint: comment out unused analyticsAPI import

--- client/src/pages/Dashboard.js
+++ client/src/pages/Dashboard.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import { useAuth } from '../context/AuthContext';
-import { analyticsAPI } from '../services/api';
+// import { analyticsAPI } from '../services/api'; // TODO: Uncomment when connecting backend
 import { 
   FiImage, 
   FiFilm, 
